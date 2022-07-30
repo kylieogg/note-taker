c73263f26425a49935b474e6bb5a094cc32f5d48
kylieogg <kylieogg1@gmail.com>
2022-07-30 16:50:04 -0400

    updated notesRoutes with post route

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -8,6 +8,18 @@ router.get('/db', (req, res) => {
     res.json(results);
   });
 
+  router.post('/notes', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = notes.length.toString();
+
+    if (!validateNote(req.body)) {
+      res.status(400).send('The note is not properly formatted.');
+    } else {
+      const note = addNote(req.body, notes);
+      res.json(note);
+    }
+  });
+
 
 
 module.exports = router;
