0f496d10262480e2fda3fa645a06870aa9f9a91b
kylieogg <kylieogg1@gmail.com>
2022-07-30 15:11:04 -0400

    added additional items to server.js

--- server.js
+++ server.js
@@ -2,11 +2,19 @@ const express = require('express');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 const fs = require('fs');
+const path = require('path');
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
 
-
+// Use apiRoutes
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 
 app.listen(PORT, () => {
