9570e29ee8cd5a44ec26d1aff249dbdaa3f71280
kylieogg <kylieogg1@gmail.com>
2022-07-30 19:45:58 -0400

    making sure I have saved what I have done so far

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { addNote } = require('../../lib/notes');
 const { notes } = require('../../db/db.json');
 
-router.get('/db', (req, res) => {
+router.get('/notes', (req, res) => {
     let results = notes;
 
     res.json(results);
@@ -12,7 +12,7 @@ router.get('/db', (req, res) => {
     // set id based on what the next index of the array will be
     req.body.id = notes.length.toString();
 
-      const note = addNote(req.body, notes);
+      var note = addNote(req.body, notes);
       res.json(note);
   });
 
